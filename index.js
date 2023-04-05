f35f79703ccb79b6b9df807541287913e99fe38b
Daniel Lee <dlee923@gmail.com>
2023-04-05 16:18:23 -0400

    added functions for creating and saving user and exercise data.  Created query function for users

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config()
+let bodyParser = require('body-parser')
 
 app.use(cors())
 app.use(express.static('public'))
@@ -19,6 +20,7 @@ const userSchema = new mongoose.Schema({
 });
 
 const exerciseSchema = new mongoose.Schema({
+  uid: String,
   username: String,
   description: {type: String, require: true},
   duration: {type: Number, require: true},
@@ -29,6 +31,64 @@ const exerciseSchema = new mongoose.Schema({
 const UserModel = mongoose.model('UserModel', userSchema);
 const ExerciseModel = mongoose.model('ExerciseModel', exerciseSchema);
 
+function createAndSaveNewUser(username) {
+  let newUser = UserModel({
+    username: username
+  });
+  newUser.save((err, newUserData) => {
+    if (err !== null) {
+      console.log('Saving new user ' + newUserData.username + ': Error...')
+    } else {
+      console.log('Saving new user ' + newUserData.username + ': Success...')
+    }
+  })
+}
+
+function createAndAddExercisesTo(userID, exerciseObj) {
+  exerciseObj.save((err, exerciseData) => {
+    if (err !== null) {
+      console.log('Saving exercise ' + exerciseObj.description + ': Error...')
+    } else {
+      console.log('Saving exercise ' + exerciseObj.description + ': Success...')
+    }
+  })
+}
+
+// enable bodyparser
+app.use('/', bodyParser.urlencoded({ extended: false }));
+
+// post API endpoints
+app.post('/api/users', function(req, res) {
+  let newUsername = req.body.username;
+  createAndSaveNewUser(newUsername);
+});
+
+app.post('api/users/:_id/exercises', function(req, res) {
+  let uid = req.body[":_id"];
+  let description = req.body.description;
+  let duration = req.body.duration;
+  let date = req.body.date;
+  let newExerciseObj = {
+    uid: uid,
+    username: '',
+    description: description,
+    duration: duration,
+    date: date
+  }
+  createAndAddExercisesTo(uid, newExerciseObj);
+});
+
+// get API endpoints
+app.get('/api/users', function(req, res) {
+  UserModel.find({}, function(err, usernameData) {
+    if (err !== null) {
+      console.log('Query users: Error...')
+    } else {
+      console.log('Query users: Success...')
+      res.json({users: usernameData})            
+    }
+  })
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
