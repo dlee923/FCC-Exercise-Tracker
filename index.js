e61c3c4fe4a81eaeb653f8d54366969bd3b4ddab
Daniel Lee <dlee923@gmail.com>
2023-04-05 12:18:39 -0400

    Added MongoDB schemas and models

--- index.js
+++ index.js
@@ -10,8 +10,24 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// connect MongoDB mongoose
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// MongoDB Schemas
+const userSchema = new mongoose.Schema({
+  username: String
+});
 
+const exerciseSchema = new mongoose.Schema({
+  username: String,
+  description: {type: String, require: true},
+  duration: {type: Number, require: true},
+  date: Date
+});
+
+// MongoDB Models
+const UserModel = mongoose.model('UserModel', userSchema);
+const ExerciseModel = mongoose.model('ExerciseModel', exerciseSchema);
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
